fccec7107ba6bb51df09452efcd6280d94aa9e3c
Riku Rasilainen <riku.rasilainen@gmail.com>
2020-09-02 21:05:48 +0300

    kurssitiedot part 1.1. completed, pushing to git for practice

--- part1/kurssitiedot/src/index.js
+++ part1/kurssitiedot/src/index.js
@@ -10,37 +10,44 @@ const App = () => {
   const part3 = 'State of a component'
   const exercises3 = 14
 
-  const Header = () => {
+  return (
     <div>
+      <Header course={course} />
+      <Content part1={part1} exercises1={exercises1} />
+      <Content part2={part2} exercises2={exercises2} />
+      <Content part3={part3} exercises3={exercises3} />
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
     </div>
-  }
+  )
+}
 
-  const Content = () => {
+const Header = (props) => {
+  return (
     <div>
+      {props.course}
     </div>
+  )
 
-    const Total = () => {
-      <div>
-      </div>
+}
 
+//Tähän tulee PARTS
 
+const Content = (props) => {
+  return (
+    <div>
+      <p>{props.part1} {props.exercises1}</p>
+      <p>{props.part2} {props.exercises2}</p>
+      <p>{props.part3} {props.exercises3}</p>
+    </div>
+  )
+}
 
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    </div>
+  )
+}
 
-      return (
-        <div>
-          <Header course={course} />
-          <p>
-            {part1} {exercises1}
-          </p>
-          <p>
-            {part2} {exercises2}
-          </p>
-          <p>
-            {part3} {exercises3}
-          </p>
-          <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-        </div>
-      )
-    }
-
-    ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))

b9420998cc2f651bf515315053ea984a08023402
Riku Rasilainen <riku.rasilainen@gmail.com>
2020-09-03 09:33:22 +0300

    kurssitiedot step 2 commit

--- part1/kurssitiedot/src/index.js
+++ part1/kurssitiedot/src/index.js
@@ -13,10 +13,9 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} exercises1={exercises1} />
-      <Content part2={part2} exercises2={exercises2} />
-      <Content part3={part3} exercises3={exercises3} />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+      <Content part1={part1} exercises1={exercises1} part2={part2}
+      exercises2={exercises2} part3={part3} exercises3={exercises3} />
+      <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   )
 }
@@ -24,20 +23,25 @@ const App = () => {
 const Header = (props) => {
   return (
     <div>
-      {props.course}
+      <h1>{props.course}</h1>
     </div>
   )
-
 }
 
-//Tähän tulee PARTS
+const Part = (props) => {
+  return (
+    <div>
+      <p>{props.part} {props.exercises}</p>
+    </div>
+  )
+}
 
 const Content = (props) => {
   return (
     <div>
-      <p>{props.part1} {props.exercises1}</p>
-      <p>{props.part2} {props.exercises2}</p>
-      <p>{props.part3} {props.exercises3}</p>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
     </div>
   )
 }
@@ -45,7 +49,7 @@ const Content = (props) => {
 const Total = (props) => {
   return (
     <div>
-      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+      <p>Number of exercises {props.total}</p>
     </div>
   )
 }
